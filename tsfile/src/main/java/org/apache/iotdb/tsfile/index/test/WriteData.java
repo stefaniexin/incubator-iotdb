073d5dd1bf12fec6ff601b61ff66fef7eb05de4f
cgf16 <cgf16@mails.tsinghua.edu.cn>
2019-05-07 17:12:13 +0800

    'adddata'

--- tsfile/src/main/java/org/apache/iotdb/tsfile/index/test/WriteData.java
+++ tsfile/src/main/java/org/apache/iotdb/tsfile/index/test/WriteData.java
@@ -1,9 +1,63 @@
 package org.apache.iotdb.tsfile.index.test;
 
+import java.io.File;
+import java.util.Random;
+import org.apache.iotdb.tsfile.file.metadata.enums.TSDataType;
+import org.apache.iotdb.tsfile.file.metadata.enums.TSEncoding;
+import org.apache.iotdb.tsfile.write.TsFileWriter;
+import org.apache.iotdb.tsfile.write.record.TSRecord;
+import org.apache.iotdb.tsfile.write.record.datapoint.DataPoint;
+import org.apache.iotdb.tsfile.write.record.datapoint.FloatDataPoint;
+import org.apache.iotdb.tsfile.write.record.datapoint.IntDataPoint;
+import org.apache.iotdb.tsfile.write.schema.MeasurementSchema;
+
 public class WriteData {
 
   public static void main(String[] args) {
 
-  }
+    try {
 
+      Random random = new Random();
+
+      String path = "test.tsfile";
+      File f = new File(path);
+      if (f.exists()) {
+        f.delete();
+      }
+      TsFileWriter tsFileWriter = new TsFileWriter(f);
+
+      // add measurements into file schema
+      for (int i = 1; i <= 5; i++) {
+        tsFileWriter
+            .addMeasurement(new MeasurementSchema("sensor_" + i, TSDataType.FLOAT, TSEncoding.RLE));
+      }
+      for (int i = 6; i <= 10; i++) {
+        tsFileWriter
+            .addMeasurement(new MeasurementSchema("sensor_" + i, TSDataType.INT32, TSEncoding.TS_2DIFF));
+      }
+
+      for (long time = 1; time <= 10000; time++) {
+
+        TSRecord tsRecord = new TSRecord(time, "device_1");
+
+        for (int i = 1; i <= 5; i++) {
+          DataPoint dPoint1 = new FloatDataPoint("sensor_" + i, 1.0f * random.nextInt(100));
+          tsRecord.addTuple(dPoint1);
+        }
+
+        for (int i = 6; i <= 10; i++) {
+          DataPoint dPoint2 = new IntDataPoint("sensor_" + i, random.nextInt(100));
+          tsRecord.addTuple(dPoint2);
+        }
+
+        tsFileWriter.write(tsRecord);
+      }
+
+      // close TsFile
+      tsFileWriter.close();
+    } catch (Throwable e) {
+      e.printStackTrace();
+      System.out.println(e.getMessage());
+    }
+  }
 }
