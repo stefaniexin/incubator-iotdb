2959c34f07df66fd92b848d12769a031d0b4e9b7
cgf16 <cgf16@mails.tsinghua.edu.cn>
2019-05-07 14:45:31 +0800

    'xx'

---
+++ iotdb/src/main/java/org/apache/iotdb/db/index/test/Main.java
@@ -0,0 +1,5 @@
+package org.apache.iotdb.db.index.test;
+
+public class Main {
+
+}
---
+++ tsfile/src/main/java/org/apache/iotdb/tsfile/index/test/WriteData.java
@@ -0,0 +1,9 @@
+package org.apache.iotdb.tsfile.index.test;
+
+public class WriteData {
+
+  public static void main(String[] args) {
+
+  }
+
+}
